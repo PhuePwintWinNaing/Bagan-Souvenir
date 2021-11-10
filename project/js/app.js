2ecbb4abbf059f98a78c288ccca3f34e66083687
Phue Pwint Win Naing <phuepwintwinnaing123@gmail.com>
2021-11-10 20:25:22 +0630

    Modified Home Page and Lacequarware Page

--- project/js/app.js
+++ project/js/app.js
@@ -1,6 +1,4 @@
 // for show sub category list
-
-
 let toggleSubCategory=()=>{
     
     let subCat = document.getElementById("subCategoryList");
@@ -12,20 +10,50 @@ let toggleSubCategory=()=>{
     }
 }
 
-$(document).ready(function(){
-    $('.slide').slick({
-        dots: true,
-infinite: true,
-speed: 300,
-slidesToShow: 1,
-adaptiveHeight: true
-});
-});
+// show items from left in item detail
+let items = ["bamboo-tea-set3.png","bamboo-tea-set10.png","bamboo-tea-set8.png","bamboo-tea-set4.png","bamboo-tea-set6.png"];
 
-$(document).ready(function(){
-    $('.slide').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-});
+let i=0;
+let leftItems=()=>{
+ i--;
+ if(i == -1)
+ {
+   i = items.length-1;
+ }
+ centerItem.src="../img/"+items[i];
+}
+
+let rightItems=()=>{
+  i++;
+  if(i == items.length)
+  {
+    i=0;
+  }
+  centerItem.src="../img/"+items[i];
+}
+
+// Change aside image to center
+let changeCenter=(i)=>{
+  let newCenterImg = i.src;
+  centerItem.src=newCenterImg;
+}
+
+// Increase and Decrease Quantity
+let decreaseQty=()=>{
+  let current = parseInt(currentQty.innerHTML);
+  // alert(current);
+  // alert(typeof current);
+  if(current != 1)
+  {
+    currentQty.innerHTML = current-1;
+  }
+  else{
+    currentQty.innerHTML = 1;
+  }
+  
+}
+
+let increaseQty=()=>{
+  let current = parseInt(currentQty.innerHTML);
+  currentQty.innerHTML = current+1;
+}
